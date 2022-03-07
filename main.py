8ca1c9fc5a9a73cd08dbd6150e59c9e49af7159e
MaciekG <maciek.1998.g@gmail.com>
2022-03-07 11:35:45 +0100

    lab2,n random_os, rating

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-
+import random
 
 def txt_to_matrix(txt_name):
     matrix = []
     with open('./data/{}.txt'.format(txt_name), 'r') as f:
-            for line in f:
-                matrix.append(list(map(str,line.split())))
+        for line in f.readlines()[1:]:
+            matrix.append(list(map(int,line.split())))
     f.close()
 
-    matrix=matrix[1:]
 
     for count, value in enumerate(matrix):
         z=count+1
@@ -25,6 +24,36 @@ def save_matrix(matrix):
                 file.write(s+'\n')
 
 
-test=txt_to_matrix('test')
+def n_pop(matrix, n):
+    m=[x for x in range(len(matrix))]
+    return [random.sample(m, len(m)) for _ in range(n)]
 
+
+def rate(matrix,n_pop):
+    rates=[]
+    for o in n_pop:
+        n=0
+        rates_uq=[]
+        try:
+            for i in o:
+                rates_uq.append(matrix[o[n]][o[n+1]])
+                n+=1
+        except:
+            rates_uq.append(matrix[o[-1]][o[0]])
+
+        rates.append(sum(rates_uq))
+    matrix_rate=[]
+    [matrix_rate.append([n_pop[i],rates[i]]) for i in range(len(n_pop))]
+    return matrix_rate
+
+
+
+
+
+
+test=txt_to_matrix('test')
+osb=n_pop(test,5)
 [print(i) for i in test]
+print(osb)
+print(rate(test,osb))
+
