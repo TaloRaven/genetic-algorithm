25b77f0fc4b8132e941821c7589fd70d36b61c95
MaciekG <maciek.1998.g@gmail.com>
2022-03-26 13:00:22 +0100

    transform class basepop to basepop and rate

--- genetic_algorithms/BasePop.py
+++ genetic_algorithms/BasePop.py
@@ -19,19 +19,3 @@ class BasePop():
         m=[x for x in range(len(self.matrix))]
         return [sample(m, len(m)) for _ in range(self.n)]
 
-    def rate(self):
-        rates=[]
-        population = self.n_pop()
-        for o in population:
-            n=0
-            rates_uq=[]
-            try:
-                for _ in o:
-                    rates_uq.append(self.matrix[o[n]][o[n+1]])
-                    n+=1
-            except:
-                rates_uq.append(self.matrix[o[-1]][o[0]])
-
-            rates.append(sum(rates_uq))
-
-        return [[population[i],rates[i]] for i in range(len(population))]
--- main.py
+++ main.py
@@ -3,26 +3,26 @@ from genetic_algorithms.Matrix import *
 from genetic_algorithms.TournamentSelection import *
 from genetic_algorithms.Pairindividuals import *
 from genetic_algorithms.PMX import *
+from genetic_algorithms.Rate import *
 def main():
     FILE_NAME='test'
 
     m1=Matrix(FILE_NAME).txt_to_matrix()
-    b1=BasePop(m1,50).rate()
-    t1=TournamentSelection(3,12,b1).sellect_winners()
-    p1=Pairindividuals(t1).pair_individuals_intercepts()
-    p1_1=Pairindividuals(t1).intercepts_individuals()
+    b1=BasePop(m1,8).n_pop()
+    r1=Rate(b1,m1).rate()
+    t1=TournamentSelection(3,8,r1).sellect_winners()
     c1=PMX(t1).c1_c2_pmx()
+
+
     print('\nMatrix \n')
     [print(x) for x in m1]
     print('\nBase population \n')
     [print(x) for x in b1]
+    print('\nRates of base population \n')
+    [print(x) for x in r1]
     print('\nTournament selecton \n')
     [print(x) for x in t1]
-    print('\nPair to cross \n')
-    [print(x) for x in p1]
-    print('\nPair to cross after ciecie \n')
-    [print(x) for x in p1_1]
-    print('\n Individuals after PMX \n')
+    print('\n PMX \n')
     [print(x) for x in c1]
 
 
