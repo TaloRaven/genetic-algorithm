c54bdc8333257aaa0f0d0a9a54aa980c3b00725d
MaciekG <maciek.1998.g@gmail.com>
2022-03-26 20:25:18 +0100

    exchange mutation added

--- genetic_algorithms/MutationExchange.py
+++ genetic_algorithms/MutationExchange.py
@@ -1,4 +1,19 @@
+from random import choice, random, sample
 class MutationExchange():
-    def __init__(self,post_cross_population) -> None:
-        self.population=post_cross_population
-        pass
+    def __init__(self,post_cross_population, mutation_rate):
+        self.post_cross_population=post_cross_population
+        self.mutation_rate=mutation_rate
+
+    def mutation(self):
+        population=self.post_cross_population
+        post_mut=[]
+        for element in population:
+            if random() < self.mutation_rate:
+                idx = range(len(element))
+                i1, i2 = sample(idx, 2)
+                element[i1], element[i2] = element[i2], element[i1]
+                post_mut.append(element)
+            else:
+                post_mut.append(element)
+        return post_mut
+
